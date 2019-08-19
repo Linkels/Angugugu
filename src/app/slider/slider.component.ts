9216c5d7bcdd63d54e660951c7a6430a52812d48
Linkels <m.chaumoitre@simplon-charleville.fr>
2019-08-19 12:12:47 +0200

    pokepush

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({selector: 'ngbd-carousel-basic', templateUrl: './slider.component.html'})
 export class NgbdCarouselBasic {
-  images = [1, 2, 3].map(() => `https://picsum.photos/1900/500?random&t=${Math.random}`);
+  images = [];
 }
 
